f8f2669ebaceae6249dc8d7719712a5579a6e349
Jagmeet Singh <emailjagmeetsingh@gmail.com>
2020-09-22 23:33:59 +0530

    added SortingComparator https://www.hackerrank.com/challenges/ctci-comparator-sorting/problem

---
+++ src/CodeCore/hackerrank/medium/SortingComparator.java
@@ -0,0 +1,48 @@
+package CodeCore.hackerrank.medium;
+
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+class Player {
+    String name;
+    int score;
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Checker implements Comparator<Player> {
+    // complete this method
+    public int compare(Player a, Player b) {
+        int comparision = Integer.compare(b.score, a.score);
+        if (comparision == 0) {
+            return a.name.compareTo(b.name);
+        } else return comparision;
+    }
+}
+
+//https://www.hackerrank.com/challenges/ctci-comparator-sorting/problem
+public class SortingComparator {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+
+        for (int i = 0; i < n; i++) {
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for (int i = 0; i < player.length; i++) {
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
